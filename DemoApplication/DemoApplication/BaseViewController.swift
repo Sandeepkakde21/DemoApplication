5f922f49020fce934c6d7d248069b0b7c8df8cff
Sandeep Kakde <sandeepkakde21@gmail.com>
2020-05-26 19:40:00 +0530

    Menu list and Add child view control completed

--- DemoApplication/DemoApplication/BaseViewController.swift
+++ DemoApplication/DemoApplication/BaseViewController.swift
@@ -8,16 +8,15 @@
 
 import UIKit
 
-class BaseViewController: UIViewController {
+class BaseViewController: UIViewController, MenuProtocol {
     @IBOutlet weak var buttonMenu: UIButton!
     @IBOutlet weak var labelTitle: UILabel!
     @IBOutlet weak var viewHeader: UIView!
     var menuVC: MenuViewController?
-    var test = "Test String"
+    var currentChildVC: UIViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         initialization()
     }
     
@@ -36,24 +35,53 @@ extension BaseViewController {
         viewHeader.backgroundColor = appThemeColor
     }
     
+    //MARK: Menu delegate
+    func menuClickedWith(index menuIndex: Int, AndTitle menuTitle: String) {
+        print(menuTitle)
+        removeChildVC()
+        currentChildVC = storyboard?.instantiateViewController(withIdentifier: "AboutCoreViewController") as? AboutCoreViewController
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4, execute: {
+            self.addChildViewControllerOnbase(childVC: self.currentChildVC)
+            
+        })
+    }
+    
     func addMenu() {
         menuVC = storyboard?.instantiateViewController(withIdentifier: "MenuViewController") as? MenuViewController
-        
+        menuVC?.delegate = self
         if let menu = menuVC {
             addChild(menu)
-            //menuVC.view.frame = frame
             view.addSubview(menu.view)
             menu.view.frame = CGRect( x: -320 , y: view.frame.origin.y, width: view.frame.size.width, height: view.frame.size.height);
-            
-            UIView.animate(withDuration: 0.7, animations: {
+            UIView.animate(withDuration: 0.3, animations: {
                 menu.view.frame = CGRect( x: 0 , y: self.view.frame.origin.y, width: self.view.frame.size.width, height: self.view.frame.size.height);
-                
             }) { (completed) in
                 menu.didMove(toParent: self)
             }
             
         } else {
             //Show  error
+        }
+    }
+    
+    //MARK: play with Child View controller
+    
+    func addChildViewControllerOnbase(childVC: UIViewController?) {
+        if let child = childVC {
+            let yAxisStart: CGFloat = 110.0
+            let totalheightForChild: CGFloat = view.frame.size.height - yAxisStart
+            addChild(child)
+            view.addSubview(child.view)
+            child.view.frame = CGRect( x: 0 , y: yAxisStart, width: view.frame.size.width, height: totalheightForChild);
+            child.didMove(toParent: self)
+        }
+    }
+    
+    func removeChildVC() {
+        if let child = currentChildVC {
+            child.willMove(toParent: nil)
+            child.view.removeFromSuperview()
+            child.removeFromParent()
         }
     }
     
--- DemoApplication/DemoApplication/MenuViewController.swift
+++ DemoApplication/DemoApplication/MenuViewController.swift
@@ -7,44 +7,48 @@
 //
 
 import UIKit
-public enum MenuTitle: String {
-    case titile1 = "title1"
-    case titile2 = "title2"
-    case titile3 = "title3"
-    case titile4 = "title4"
-}
+
+let menuTitleArray = [MENU_PERSONAL_DETAILS,MENU_CONTACT_INFO,MENU_MY_ORGANIZATION_CHART,MENU_ANNOUNCMENT,MENU_NOTIFICATION ,MENU_MY_PUBLIC_PROFILE]
+
 
 protocol MenuProtocol {
     func menuClickedWith(index menuIndex: Int, AndTitle menuTitle: String)
 }
 
-class MenuViewController: UIViewController {
+class MenuViewController: UIViewController, UIGestureRecognizerDelegate {
     var delegate: MenuProtocol?
     @IBOutlet weak var menuTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         initialization()
-        
     }
     
 }
 
 extension MenuViewController: UITableViewDataSource, UITableViewDelegate {
     
-     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return  6
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return  7
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell: UITableViewCell!
         if indexPath.row == 0 {
-         cell  = tableView.dequeueReusableCell(withIdentifier: "MenuHeaderTableViewCell", for: indexPath) as! MenuHeaderTableViewCell
+            cell  = tableView.dequeueReusableCell(withIdentifier: "MenuHeaderTableViewCell", for: indexPath) as! MenuHeaderTableViewCell
             
         } else {
-        cell = tableView.dequeueReusableCell(withIdentifier: "MenuTitleTableViewCell", for: indexPath) as! MenuTitleTableViewCell
+            let menuCell: MenuTitleTableViewCell = tableView.dequeueReusableCell(withIdentifier: "MenuTitleTableViewCell", for: indexPath) as! MenuTitleTableViewCell
+            let actualRowIndex = indexPath.row - 1
+            if menuTitleArray.count > actualRowIndex && actualRowIndex >= 0 {
+                menuCell.cellConfiguration(row: actualRowIndex, menuTitle: menuTitleArray[actualRowIndex])
+                menuCell.menuTitleButton.tag = actualRowIndex
+                menuCell.menuTitleButton.addTarget(self, action: #selector(menuClicked(button:)), for: .touchUpInside)
+            }
+            cell = menuCell
+            
         }
-     return cell
+        return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -55,16 +59,22 @@ extension MenuViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        delegate?.menuClickedWith(index: indexPath.row, AndTitle: MenuTitle.titile1.rawValue)
     }
     
+    @objc func menuClicked(button: UIButton) {
+        let menuTitle = menuTitleArray[button.tag]
+        delegate?.menuClickedWith(index: button.tag, AndTitle: menuTitle)
+        removeMenu()
+        
+    }
     
 }
 
 extension MenuViewController {
     func initialization() {
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.removeMenu(_:)))
-        view.addGestureRecognizer(tap)
+        let tapOnScreen: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.removeMenu(_:)))
+        tapOnScreen.delegate = self
+        view.addGestureRecognizer(tapOnScreen)
         view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
     }
     
@@ -72,13 +82,13 @@ extension MenuViewController {
         removeMenu()
     }
     
+    
     func removeMenu() {
-        UIView.animate(withDuration: 0.7, animations: {
+        UIView.animate(withDuration: 0.3, animations: {
             self.view.frame = CGRect( x: -320, y: self.view.frame.origin.y, width: self.view.frame.size.width, height: self.view.frame.size.height);
         }) { (completed) in
             self.willMove(toParent: nil)
             self.view.removeFromSuperview()
-            
             self.removeFromParent()
         }
     }
