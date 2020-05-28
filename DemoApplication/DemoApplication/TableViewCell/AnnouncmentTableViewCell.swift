63188033160b44650f246248c3b13e8b93d5e41f
Sandeep Kakde <sandeepkakde21@gmail.com>
2020-05-28 14:42:40 +0530

    Announcment, Notificato,my organization chart screen design

---
+++ DemoApplication/DemoApplication/TableViewCell/AnnouncmentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnnouncmentTableViewCell.swift
+//  DemoApplication
+//
+//  Created by Sandeep Kakde on 28/05/20.
+//  Copyright © 2020 Sandeep Kakde. All rights reserved.
+//
+
+import UIKit
+
+class AnnouncmentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DemoApplication/DemoApplication/TableViewCell/MyOrganizationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyOrganizationTableViewCell.swift
+//  DemoApplication
+//
+//  Created by Sandeep Kakde on 27/05/20.
+//  Copyright © 2020 Sandeep Kakde. All rights reserved.
+//
+
+import UIKit
+
+class MyOrganizationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
